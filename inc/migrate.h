e1c3cbd63beb57b4b86e732f25ab9da54d7b0298
James Danz <danz@fas.harvard.edu>
2011-12-10 11:24:54 -0500

    copied over new inc/migrate.h

--- inc/migrate.h
+++ inc/migrate.h
@@ -13,12 +13,16 @@
 // The port to use for sending migration requests (see lib/migrate.c)
 #define SEND_PORT 4242 
 
+#define MEM_BARRIER asm volatile("" : : : "memory")
+
 #define MIGRATE_MAGIC 0x1EFACADE
-#define MIGRATE_PG_MAGIC 0XDECADE42
+#define MIGRATE_PG_MAGIC 0xDECADE42
+#define MIGRATE_RESPONSE_MAGIC 0xD15EA5E
+#define MIGRATE_PG_REQUEST_MAGIC 0xCAFEBABE
+#define MIGRATE_PG_REQUEST_RESPONSE_MAGIC 0xDEADFACE
 
 struct MigrateSuperHeader {
 	uint32_t msh_magic; // must equal MIGRATE_MAGIC
-    uintptr_t msh_pgfault_upcall;
 	uint32_t msh_n_pages;
 	struct Trapframe msh_tf;
 }__attribute__((packed));
@@ -27,10 +31,33 @@ struct MigratePageHeader {
 	uint32_t mph_magic; // must equal MIGRATE_PG_MAGIC
 	void *mph_addr;
 	int mph_perm;
+	bool mph_mapped_remote_src; // for Page.pp_exists_on_remote_machine
+}__attribute__((packed));
+
+struct MigratePageRequest {
+	uint32_t mpr_magic; // must equal MIGRATE_PG_REQUEST_MAGIC
+	envid_t mpr_src_envid;
+	physaddr_t mpr_physaddr;
+	physaddr_t mpr_physaddr_on_requesting_machine;
+}__attribute__((packed));
+
+struct MigrateResponseHeader {
+	uint32_t mr_magic; // must equal MIGRATE_RESPONSE_MAGIC
+	envid_t mr_recipient_envid;
+}__attribute__((packed));
+
+
+struct MigratePageRequestResponseHeader {
+	uint32_t mprr_magic; // must equal MIGRATE_PG_REQUEST_RESPONSE_MAGIC
+	physaddr_t mprr_physaddr;
 }__attribute__((packed));
 
 int migrate_spawn(int f);
-int migrate();
 int migrate_locally(void (*callback)(void));
+void migrate_label();
+
+
+
+int migrate_process_page_request(int f);
 
 #endif	// !JOS_INC_MIGRATE_H
